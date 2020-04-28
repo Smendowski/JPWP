d8cbc058f8ba6bb4dcb09f19b0a6a92b2f7b8552
Smendowski <m.smendowski@gmail.com>
2020-04-28 16:50:23 +0200

    Rozwiązanie 1 zadania

--- JPWP_zadania/src/Zadanie1.java
+++ JPWP_zadania/src/Zadanie1.java
@@ -20,38 +20,42 @@ public class Zadanie1 {
         c.gridwidth = 2;
         c.gridheight = 2;
         f.add(label, c);
-
         JLabel label2 = new JLabel("ccc");
         label2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        c.gridx = ?;
-        c.gridy = ?;
-        c.gridwidth = ?;
-        c.gridheight = ?;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
         f.add(label2, c);
-
         JLabel label3 = new JLabel("ddd");
         label3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        c.gridx = ?;
-        c.gridy = ?;
-        c.gridwidth = ?;
-        c.gridheight = ?;
+        c.gridx = 2;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
         f.add(label3, c);
 
         JLabel label4 = new JLabel("aaa");
         label4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        c.gridx = ?;
-        c.gridy = ?;
-        c.gridwidth = ?;
-        c.gridheight = ?;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
         f.add(label4, c);
-
         JLabel label5 = new JLabel("bbb");
         label5.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        c.gridx = ?;
-        c.gridy = ?;
-        c.gridwidth = ?;
-        c.gridheight = ?;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
         f.add(label5, c);
+        JButton button = new JButton("button");
+        button.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        c.gridx = 2;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        f.add(button, c);
 
 
         f.pack();
