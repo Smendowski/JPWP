f249e2da3cf1823e7d2cd2f665c116f7c22b5b4e
Smendowski <m.smendowski@gmail.com>
2020-05-19 10:58:45 +0200

    Aktualizacja dla Zadania 2

--- Zad1_Trading/data_storage.py
+++ Zad1_Trading/data_storage.py
@@ -48,9 +48,19 @@ class DataStorage:
 		stock = self.data.get(symbol)
 
 		if stock.position == 1:
+			# DLA ZADANIA 2:
+			# Przed zakupem należy zamknąć sprzedaż
+			self.close_sell(symbol, ask)
 			self.open_buy(symbol, ask)
+	
+
 		if stock.position == -1:
+			# DLA ZADANIA 2:
+			# Po zamknięciu sprzedaży rozpoczynami zakupy
 			self.close_buy(symbol, bid)
+			self.open_sell(symbol, bid)
+			
+			
 
 	def open_buy(self, symbol, price):
 		""" Open long position.
@@ -100,6 +110,56 @@ class DataStorage:
 		}
 		response = self.command_execute('tradeTransaction', transaction)
 		print('Sell ', symbol, ' for ', price, ', status: ', response['status'])
+
+	# Zadanie 2
+ 	def open_sell(self, symbol, price):
+ 		# Transakcja. Transaction side SELL, oraz Order OPEN:
+ 		transaction = {
+            "tradeTransInfo": {
+                "cmd": xAPIConnector.TransactionSide.SELL,
+                "order": 0,
+                "price": price,
+                "symbol": symbol,
+                "type": xAPIConnector.TransactionType.ORDER_OPEN,
+                "volume": 1
+            }
+        }
+
+        # Analogicznie do metody open_buy, wykonanie transakcji dzięki
+        # command_execute	
+        response = self.command_execute('tradeTransaction', transaction)
+		print('Open Sell ', symbol, ' for ', price, ', status: ', response['status'])
+
+ 	def close_sell(self, symbol, price):
+
+		# List opened positions
+		transaction = {
+			"openedOnly": True
+		}
+		trades = self.command_execute('getTrades', transaction)
+		# Get latest position
+		for trade in trades['returnData']:
+			if trade['symbol'] == symbol:
+				last_position = trade
+				break
+		# Extract order ID
+		order = last_position['order']
+
+ 		# Transakcja. Transaction side SELL, oraz Order CLOSE:
+ 		transaction = {
+            "tradeTransInfo": {
+                "cmd": xAPIConnector.TransactionSide.SELL,
+                "order": 0,
+                "price": price,
+                "symbol": symbol,
+                "type": xAPIConnector.TransactionType.ORDER_CLOSE
+                "volume": 1
+            }
+        }
+
+        response = self.command_execute('tradeTransaction', transaction)
+		print('Close Sell ', symbol, ' for ', price, ', status: ', response['status'])
+
 
 	def raport(self):
 		for symbol, obj in self.data.items():
